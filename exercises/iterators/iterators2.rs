752045463ecacb410d6a7de8e6a93376af8fb472
fnnx1 <FMxi66@outlook.com>
2024-10-17 20:37:37 +0800

    msg

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,7 +6,6 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -15,7 +14,7 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => first.to_uppercase().collect::<String>() + c.as_str(),
     }
 }
 
@@ -23,16 +22,46 @@ pub fn capitalize_first(input: &str) -> String {
 // Apply the `capitalize_first` function to a slice of string slices.
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
-pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
-}
+pub fn capitalize_words_vector(words: &[ &str]) -> Vec<String> {
+    let mut wordss =Vec::new();
+    let mut y = wordss.iter();
+    for x in words {
+        let y = x.to_string();
+        let mut y = y.chars();
+
+        wordss.push(
+             match y.next() {
+            None => String::new(),
+            Some(first) => first.to_uppercase().collect::<String>() + y.as_str(),
+            }
+        );
+    }
+    wordss
+    }
 
 // Step 3.
 // Apply the `capitalize_first` function again to a slice of string slices.
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut wordss =Vec::new();
+    let mut y = wordss.iter();
+    let mut wordsss=String::new();
+    for x in words {
+        let y = x.to_string();
+        let mut y = y.chars();
+
+        wordss.push(
+             match y.next() {
+                None => String::new(),
+                Some(first) => first.to_uppercase().collect::<String>() + y.as_str(),
+            }
+        );
+    }
+    for xx in wordss {
+        wordsss += &xx;
+    }
+    wordsss
 }
 
 #[cfg(test)]
