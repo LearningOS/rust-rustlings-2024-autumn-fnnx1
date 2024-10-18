cf7a9b89373b23753f6b5ca10cea6ffd219f7350
fnnx1 <FMxi66@outlook.com>
2024-10-18 10:59:23 +0800

    msg

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,18 +3,24 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
+    let mut i = 1;
+    let mut k=1;
+    while i<num&&k!=0 {
+        k=k*(i+1);
+        i+=1;
+    }
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
-    // Try not to use:
+    // Try not to use:dsgv
     // - imperative style loops (for, while)
     // - additional variables
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    k
 }
 
 #[cfg(test)]
